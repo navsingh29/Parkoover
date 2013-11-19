6f8b5cfc57119fb7a94e3eed0f7244fba4252017
Kexin Lu <langhxs@hotmail.com>
2013-11-19 19:29:33 +0800

    (4) Now we can plot route by(String zipcode, LatLng destination,String transportationMethod) (5) Now we can enter street view by calling the method goToStreetViewAtPoint(Double x, Double y) at position x,y
    Note: Still having trouble to move the camera after plotting the route.
          Still working on exit streetView.

--- src/com/grasshoppers/parkfinder/client/GUI/widgets/GoogleMapsWidget.java
+++ src/com/grasshoppers/parkfinder/client/GUI/widgets/GoogleMapsWidget.java
@@ -3,6 +3,9 @@ package com.grasshoppers.parkfinder.client.GUI.widgets;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Label;
@@ -15,26 +18,43 @@ import com.google.maps.gwt.client.DirectionsRequest;
 import com.google.maps.gwt.client.DirectionsResult;
 import com.google.maps.gwt.client.DirectionsService;
 import com.google.maps.gwt.client.DirectionsStatus;
+import com.google.maps.gwt.client.Geocoder;
+import com.google.maps.gwt.client.GeocoderRequest;
+import com.google.maps.gwt.client.GeocoderResult;
+import com.google.maps.gwt.client.GeocoderStatus;
 import com.google.maps.gwt.client.GoogleMap;
 import com.google.maps.gwt.client.LatLng;
 import com.google.maps.gwt.client.MapOptions;
 import com.google.maps.gwt.client.MapTypeId;
 import com.google.maps.gwt.client.Marker;
 import com.google.maps.gwt.client.MarkerOptions;
+import com.google.maps.gwt.client.StreetViewPanorama;
+import com.google.maps.gwt.client.StreetViewPanoramaOptions;
 import com.google.maps.gwt.client.TravelMode;
-
 public class GoogleMapsWidget extends Composite{
 	
 	private SimplePanel sp;
 	GoogleMap theMap;
 	LatLng lastPark;
+	DirectionsService dService;
+	DirectionsRequest dRequest;
+	DirectionsRenderer renderer;
+	LatLng geocodeResult;
+    StreetViewPanoramaOptions svpo;
+
+	
 	public GoogleMapsWidget(){
+		 dService = DirectionsService.create();
+		 dRequest = DirectionsRequest.create();
 		VerticalPanel vp = new VerticalPanel();
 		
 		lastPark = LatLng.create(49.2500,-123.100);
 		sp = new SimplePanel();
 		sp.setWidth("300px");
 		sp.setHeight("300px");
+		
+		renderer = DirectionsRenderer.create();
+		svpo = StreetViewPanoramaOptions.create();
 		
 		MapOptions option = MapOptions.create();
 		option.setCenter(LatLng.create(49.2500, -123.1000));
@@ -93,12 +113,11 @@ public class GoogleMapsWidget extends Composite{
         mo.setPosition(LatLng.create(x, y));
         Marker marker = Marker.create(mo);
         marker.setMap(theMap);
+       
 		
 	}
-	 
+	
 	public void plotRoute(Double x1, Double y1, Double x2, Double y2, String travelMethodWorBorD){
-		DirectionsService dService = DirectionsService.create();
-		DirectionsRequest dRequest = DirectionsRequest.create();
 		TravelMode travelMethod = TravelMode.DRIVING;
 		
 		lastPark = LatLng.create(x1, x2);
@@ -121,9 +140,44 @@ public class GoogleMapsWidget extends Composite{
 			
 			@Override
 			public void handle(DirectionsResult a, DirectionsStatus b) {
-				DirectionsRendererOptions directionsRendererOption = DirectionsRendererOptions.create();
-				directionsRendererOption.setDirections(a);
-				DirectionsRenderer renderer=  DirectionsRenderer.create(directionsRendererOption);
+				//DirectionsRendererOptions directionsRendererOption = DirectionsRendererOptions.create();
+				//directionsRendererOption.setDirections(a);
+				renderer.setDirections(a);
+			    //renderer=  DirectionsRenderer.create(directionsRendererOption);
+				renderer.setMap(theMap);
+			}
+		});
+		
+		
+		
+		
+	}
+	public void plotRouteWithLatL(LatLng orign, LatLng des, String travelMethodWorBorD){
+		TravelMode travelMethod = TravelMode.DRIVING;
+		lastPark = orign;
+		
+		if(travelMethodWorBorD == "W"){
+		     travelMethod = TravelMode.WALKING;
+				}
+		if(travelMethodWorBorD == "B"){
+			 travelMethod = TravelMode.BICYCLING;
+		}
+		if(travelMethodWorBorD == "D"){
+			 travelMethod = TravelMode.DRIVING;
+		}
+		
+		dRequest.setDestination(des);
+		dRequest.setOrigin(orign);
+		dRequest.setTravelMode(travelMethod);
+		
+		dService.route(dRequest, new DirectionsService.Callback() {
+			
+			@Override
+			public void handle(DirectionsResult a, DirectionsStatus b) {
+				//DirectionsRendererOptions directionsRendererOption = DirectionsRendererOptions.create();
+				//directionsRendererOption.setDirections(a);
+				renderer.setDirections(a);
+			    //renderer=  DirectionsRenderer.create(directionsRendererOption);
 				renderer.setMap(theMap);
 			}
 		});
@@ -157,4 +211,43 @@ public class GoogleMapsWidget extends Composite{
 		}
 	}
 	
+	public void panTo(Double x,Double y){
+        renderer = DirectionsRenderer.create();
+        theMap.panTo(LatLng.create(x,y));
+	}
+	
+	
+	public void plotRouteFromHomeToPark(String zipcode,String city,Double x,Double y, String transMethod){
+	    
+		final String address = zipcode + "," + city;
+		final LatLng dest = LatLng.create(x, y);
+		final String transM = transMethod;
+		Geocoder geocoder = Geocoder.create();
+		GeocoderRequest geoRequest = GeocoderRequest.create();
+		geoRequest.setAddress(address);
+		
+		geocoder.geocode(geoRequest, new Geocoder.Callback() {
+			
+			@Override
+			public void handle(JsArray<GeocoderResult> a, GeocoderStatus b) {
+			if (b == GeocoderStatus.OK){
+				GeocoderResult result = a.shift();
+				plotRouteWithLatL(result.getGeometry().getLocation(), dest, transM);
+				
+			}else{
+				Window.alert("Fail to find user's address, please check your profile");
+			}	}
+		});
+		{}
+	}
+	
+	public void goToStreetViewAtPoint(Double x, Double y){
+		
+		  
+		 svpo.setPosition(LatLng.create(x, y));
+		 svpo.setVisible(true);
+		 StreetViewPanorama svpd = StreetViewPanorama.create(getElement(), svpo);
+	}
+	
+
 }
